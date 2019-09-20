1240b45ff59e6eb460f8d4dce47c62e06eddc5df
Ethan Hoover <51389138+Cireimu@users.noreply.github.com>
2019-09-20 09:33:38 -0700

    MVP | Some Stretch

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(arg1, arg2, cb) {
+  console.log(cb(arg1,arg2));
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,12 +17,26 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+let add = function(arg1,arg2){
+  return arg1 + arg2;
+}
+
+let multiply = function(arg1,arg2){
+  return arg1 * arg2;
+}
+
+let greeting = function(first,last){
+  return `Hello ${first} ${last}, nice to meet you!` ;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // console.log(consume(2, 2, add)); // 4
 // console.log(consume(10, 16, multiply)); // 160
 // console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
+consume(2,2,add); 
+consume(10,16,multiply); 
+consume("Mary","Poppins", greeting); 
 
 // ==== Closures ==== 
 
